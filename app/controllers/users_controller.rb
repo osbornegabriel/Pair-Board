f1a43849d42ad418cd6296ce85e793fc3c4c3069
Erik Wotring <ewotring@gmail.com>
2017-08-31 12:45:37 -0500

    WIP user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   end
 
   def show
+  	@user = User.find(params[:id])
   end
 
   def update
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: "Steve", last_name: "Smith", email: "steve@steve.com", password: "password", phone: "1234567890", picture_url: "http://thumbs.dreamstime.com/x/young-owl-wearing-hat-16562958.jpg", bio: "I'm a bird", topics: ["automatic dishwashers", "windshield wipers"])
