216f72d226c43341cedadf47bca72255cc2c1400
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-31 12:16:03 -0500

    Create proper inputs for new slot form

--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -1,5 +1,17 @@
 class SlotsController < ApplicationController
 
+  def new
+    @slot = Slot.new
+  end
 
+  def create
+    @slot = Slot.new(slot_params)
+
+    if @slot.save
+
+    else
+
+    end
+  end
 
 end
--- db/migrate/20170830212324_create_appointments.rb
+++ db/migrate/20170830212324_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[5.1]
   def change
     create_table :appointments do |t|
       t.datetime :start_time, null: false
-      t.integer :appointment_id, null: false
+      t.integer :slot_id, null: false
       t.integer :mentee_id
 
       t.timestamps
