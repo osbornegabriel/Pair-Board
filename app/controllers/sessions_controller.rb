9599f15696bffc0f2d42c3a6442a1b92386f9094
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-31 10:42:30 -0500

    Create controllers, add routes in router, and write user controller actions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'sessions#new'
+
+  resources :sessions, only: [:new, :create, :delete]
+  resources :slots do
+    resources :appointments
+  end
+  resources :users, only: [:new, :create, :show, :update]
+
+
 end
