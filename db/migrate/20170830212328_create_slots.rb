473ccf96a74a034e6babb3edbf0d429744f06b45
Gabriel Osborne <osbornegabriel@gmail.com>
2017-08-31 10:27:24 -0500

    Reverse naming for appointments and slots to match new schema naming

--- db/migrate/20170830212328_create_slots.rb
+++ db/migrate/20170830212328_create_slots.rb
@@ -2,8 +2,7 @@ class CreateSlots < ActiveRecord::Migration[5.1]
   def change
     create_table :slots do |t|
       t.datetime :start_time, null: false
-      t.integer :appointment_id, null: false
-      t.integer :mentee_id
+      t.integer :mentor_id, null: false
 
       t.timestamps
     end
