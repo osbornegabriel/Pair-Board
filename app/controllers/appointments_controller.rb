7c7b8a8e88b41f404a376aa913f3ae8cb39367c5
Ravi <i.rrpatel@gmail.com>
2017-09-01 14:16:34 -0500

    New appointments saving without ajax

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,5 +3,15 @@ class AppointmentsController < ApplicationController
   end
 
   def create
+    @slot = Slot.find(params[:slot_id])
+
+    @appointment = Appointment.new(mentee_id: params[:mentee_id], slot_id: @slot.id)
+    if @appointment.save
+      redirect_to slot_path(@slot)
+      "CONFIRMED!"
+    else
+      @errors = @appointment.errors.full_messages
+      render 'slots/show'
+    end
   end
 end
