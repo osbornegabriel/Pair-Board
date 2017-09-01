c0d684b6c2bf06c136994af121a93dfaefb9ece3
Erik Wotring <ewotring@gmail.com>
2017-09-01 13:00:37 -0500

    Copy changes from another branch

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,26 +9,34 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
       redirect_to '/'
     else
-      p '8' * 88
-      p @user.errors.full_messages
       render 'new'
     end
   end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    # if @user.update()
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    render "edit"
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :phone)
   end
 
+  def topic_params
+    p"*"*90
+    p params.require(:user).permit(:topic1, :topic2, :topic3)
+  end
 end
